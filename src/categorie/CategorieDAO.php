ba40ff04bff03104e207c9daf23f5d6da0b56f20
redes19 <120016236+redes19@users.noreply.github.com>
2023-11-30 12:34:13 +0100

    refactor categorie

--- src/categorie/CategorieDAO.php
+++ src/categorie/CategorieDAO.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(Categorie.php);
+
 class CategorieDAO{
     private $db;
 
@@ -7,32 +9,47 @@ class CategorieDAO{
         $this->db = $db;
     }
 
-     // CREATE
-     public function ajouterCategorie($nomCategorie) {
+    // CREATE
+    public function ajouterCategorie(Categorie $categorie) {
+        $nomCategorie = $categorie->getNomCategorie();
+    
         $query = "INSERT INTO categories (nom_categorie) VALUES (:nomCategorie)";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':nomCategorie', $nomCategorie, PDO::PARAM_STR);
         return $stmt->execute();
     }
-
+    
     // READ
     public function getAllCategories() {
         $query = "SELECT * FROM categories";
         $stmt = $this->db->prepare($query);
+        $categories = [];
+
         $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach($stmt as $row){
+            $categorie = new Categorie($row['categorie_id'], $row['nom_categorie']);
+            $categories[] = $categorie;
+        }
+        return $categories;
     }
 
     public function getCategorieById($categorieId) {
         $query = "SELECT * FROM categories WHERE categorie_id = :categorieId";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':categorieId', $categorieId, PDO::PARAM_INT);
+
         $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return new Categorie($row['categorie_id'], $row['nom_categorie']);
     }
 
     // UPDATE
-    public function modifierCategorie($categorieId, $nouveauNom) {
+    public function modifierCategorie(Categorie $categorie) {
+        $categorieId = $categorie->getCategorieId();
+        $nouveauNom = $categorie->getNomCategorie();
+
         $query = "UPDATE categories SET nom_categorie = :nouveauNom WHERE categorie_id = :categorieId";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':categorieId', $categorieId, PDO::PARAM_INT);
@@ -47,6 +64,7 @@ class CategorieDAO{
         $stmt->bindParam(':categorieId', $categorieId, PDO::PARAM_INT);
         return $stmt->execute();
     }
+
 
 }
 
