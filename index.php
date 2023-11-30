fcfe91df25c9eaf7af17d03a4392668d211ef877
redes19 <120016236+redes19@users.noreply.github.com>
2023-11-30 23:20:16 +0100

    test index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saint recette</title>
+    <link href="/Css/index.css">
+</head>
+<body>
+    <h1>coucou</h1>
+    <h2>salut</h2>
+</body>
+</html>
