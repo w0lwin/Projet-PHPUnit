7a85c0a85280ed59fbe7486bbec738d0374a39ad
w0lwin <teo.bacher@hotmail.com>
2023-11-30 16:44:20 +0100

    test / update, add recetteDAO / testRecette
    sur la fonction addRecette
    getRecetteById

--- src/recette/RecetteDAO.php
+++ src/recette/RecetteDAO.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Recette.php';
+
 class RecetteDAO{
 
     private $pdo;
@@ -8,25 +10,99 @@ class RecetteDAO{
         $this->pdo = $pdo;
     }
 
-    public function recetteById($id){
-        $stmt = $this->pdo->prepare("SELECT * FROM recette WHERE id = :id");
+    public function getRecetteById($id){
+
+        if ($id == null){
+            throw new InvalidArgumentException('id should not be null');
+        }
+
+        if (!is_int($id)){
+            throw new InvalidArgumentException('id should be an integer');
+        }
+
+        $stmt = $this->pdo->prepare("SELECT * FROM recettes WHERE id = :id");
         $stmt->execute(['id' => $id]);
-        $row = $stmt->fetch();
-        return new Recette($row['id'], $row['nom_recette'], $row['instruction'], $row['temps_preparation'], $row['temps_cuisson'], $row['difficulte'], $row['categorie_id']);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $recette = new Recette($result['id'], $result['nom_recette'], $result['instruction'], $result['temps_preparation'], $result['temps_cuisson'], $result['difficulte'], $result['categorie_id'], $result['ingredients']);
+        return $recette;
     }
 
-    public function addRecette($nom_recette, $instruction, $temps_preparation, $temps_cuisson, $difficulte, $categorie_id){
-        $stmt = $this->pdo->prepare("INSERT INTO recette (nom_recette, instruction, temps_preparation, temps_cuisson, difficulte, categorie_id) VALUES (:nom_recette, :instruction, :temps_preparation, :temps_cuisson, :difficulte, :categorie_id)");
-        $stmt->execute(['nom_recette' => $nom_recette, 'instruction' => $instruction, 'temps_preparation' => $temps_preparation, 'temps_cuisson' => $temps_cuisson, 'difficulte' => $difficulte, 'categorie_id' => $categorie_id]);
+    public function getRecettes(){
+        $stmt = $this->pdo->prepare("SELECT * FROM recettes");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $recettes = [];
+        foreach ($result as $row) {
+            $ingredients = $this->getIngredientsRecette($row['id']);
+            foreach ($ingredients as $ingredient) {
+                $ingredients[] = $ingredient['id'];
+            }
+            $recette = new Recette($row['id'], $row['nom_recette'], $row['instruction'], $row['temps_preparation'], $row['temps_cuisson'], $row['difficulte'], $row['categories_id'], $ingredients);
+            array_push($recettes, $recette);
+        }
+        return $recettes;
     }
+    
+
+    public function getIngredientsRecette($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM recette_ingredients WHERE recette_id = :id");
+        $stmt->execute(['id' => $id]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $ingredients = [];
+        foreach ($result as $row) {
+            $ingredient = ['id' => $row['ingredient_id'], 'quantite' => $row['quantite']];
+            array_push($ingredients, $ingredient);
+        }
+        return $ingredients;
+    }
+    
+
+    public function addRecette(Recette $recette)
+    {   
+        $nom_recette = $recette->getNomRecette();
+        $instruction = $recette->getInstruction();
+        $temps_preparation = $recette->getTempsPreparation();
+        $temps_cuisson = $recette->getTempsCuisson();
+        $difficulte = $recette->getDifficulte();
+        $categorie_id = $recette->getCategorieId();
+        
+        
+        if ($nom_recette == null || $instruction == null || $temps_preparation == null || $temps_cuisson == null || $difficulte == null || $categorie_id == null){
+            throw new InvalidArgumentException('nom_recette should not be null');
+        }
+
+        if (!is_int($temps_preparation) || !is_int($temps_cuisson) || !is_int($difficulte)) {
+            throw new InvalidArgumentException('temps_preparation, temps_cuisson and difficulte should be integers');
+        }
+        
+        foreach ($this->getRecettes() as $recette) {
+            if ($recette->getNomRecette() == $nom_recette) {
+                throw new InvalidArgumentException('nom_recette should be unique');
+            }
+        }
+
+        $stmt = $this->pdo->prepare("INSERT INTO recettes (nom_recette, instruction, temps_preparation, temps_cuisson, difficulte, categories_id) VALUES (:nom_recette, :instruction, :temps_preparation, :temps_cuisson, :difficulte, :categorie_id)");
+        $stmt->execute(['nom_recette' => $nom_recette, 'instruction' => $instruction, 'temps_preparation' => $temps_preparation, 'temps_cuisson' => $temps_cuisson, 'difficulte' => $difficulte, 'categorie_id' => $categorie_id]);
+    
+        //recupère l'id de la dernière recette ajouté
+        $recette_id = $this->pdo->lastInsertId();
+        $ingredients = $recette->getIngredients();
+
+        foreach ($ingredients as $ingredient) {
+            $stmt2 = $this->pdo->prepare("INSERT INTO recette_ingredients (recette_id, ingredient_id, quantite) VALUES (:recette_id, :ingredient_id, :quantite)");
+            $stmt2->execute(['recette_id' => $recette_id, 'ingredient_id' => $ingredient['id'], 'quantite' => $ingredient['quantite']]);
+        }
+    }
+    
 
     public function updateRecette($id, $nom_recette, $instruction, $temps_preparation, $temps_cuisson, $difficulte, $categorie_id){
-        $stmt = $this->pdo->prepare("UPDATE recette SET nom_recette = :nom_recette, instruction = :instruction, temps_preparation = :temps_preparation, temps_cuisson = :temps_cuisson, difficulte = :difficulte, categorie_id = :categorie_id WHERE id = :id");
+        $stmt = $this->pdo->prepare("UPDATE recettes SET nom_recette = :nom_recette, instruction = :instruction, temps_preparation = :temps_preparation, temps_cuisson = :temps_cuisson, difficulte = :difficulte, categorie_id = :categorie_id WHERE id = :id");
         $stmt->execute(['id' => $id, 'nom_recette' => $nom_recette, 'instruction' => $instruction, 'temps_preparation' => $temps_preparation, 'temps_cuisson' => $temps_cuisson, 'difficulte' => $difficulte, 'categorie_id' => $categorie_id]);
     }
 
     public function deleteRecette($id){
-        $stmt = $this->pdo->prepare("DELETE FROM recette WHERE id = :id");
+        $stmt = $this->pdo->prepare("DELETE FROM recettes WHERE id = :id");
         $stmt->execute(['id' => $id]);
     }
 
---
+++ tests/integration/RecetteTest.php
@@ -0,0 +1,111 @@
+<?php
+require_once 'src/recette/RecetteDAO.php';
+require_once 'src/ingredient/IngredientDAO.php';
+
+use PHPUnit\Framework\TestCase;
+
+class RecetteTest extends TestCase
+{
+
+    private $pdo;
+    private $recetteDAO;
+    private $ingredientDAO;
+
+    protected function setUp(): void
+    {
+        $this->configDatabase();
+        $this->recetteDAO = new RecetteDAO($this->pdo);
+        $this->ingredientDAO = new IngredientDAO($this->pdo);
+    }
+
+    private function configDatabase(): void
+    {
+        $this->pdo = new PDO(
+            sprintf(
+                'mysql:host=%s;port=%s;dbname=%s',
+                getenv('DB_HOST'),
+                getenv('DB_PORT'),
+                getenv('DB_DATABASE')
+            ),
+            getenv('DB_USERNAME'),
+            getenv('DB_PASSWORD')
+        );
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function testAddRecette( )
+    {
+        $nomRecette = 'Rougail saucisse';
+        $instruction = 'Faire cuire le poulet dans une poele';
+        $tempsPreparation = 10;
+        $tempsCuisson = 20;
+        $difficulte = 1;
+        $categorie = 1;
+        $ingredients = [2, 4];
+        
+       
+
+        if ($nomRecette == null || $instruction == null || $tempsPreparation == null || $tempsCuisson == null || $difficulte == null || $ingredients == null) {
+            $this->expectException(Exception::class);
+        }
+
+        if (!is_int($tempsPreparation) || !is_int($tempsCuisson) || !is_int($difficulte)) {
+            $this->expectException(Exception::class);
+        }
+
+        foreach ($this->recetteDAO->getRecettes() as $recette) {
+            if ($recette->getNomRecette() == $nomRecette) {
+                $this->expectException(Exception::class);
+            }
+        }
+
+        $ingredientsRecette = [];
+        foreach ($ingredients as $ingredient) {
+            $ingredientsRecette[] = $this->ingredientDAO->getIngredientsById($ingredient);
+        }
+
+
+        $recette = new Recette(null, $nomRecette, $instruction, $tempsPreparation, $tempsCuisson, $difficulte,  $categorie, $ingredientsRecette);
+        $this->recetteDAO->addRecette($recette);
+
+        $addedRecette = $this->recetteDAO->getRecetteById(3);
+
+        $this->assertEquals(3, $addedRecette->getId());
+        $this->assertEquals($recette->getNomRecette(), $addedRecette->getNomRecette());
+        $this->assertEquals($recette->getInstruction(), $addedRecette->getInstruction());
+        $this->assertEquals($recette->getTempsPreparation(), $addedRecette->getTempsPreparation());
+        $this->assertEquals($recette->getTempsCuisson(), $addedRecette->getTempsCuisson());
+        $this->assertEquals($recette->getDifficulte(), $addedRecette->getDifficulte());
+    }
+
+
+
+
+
+    // public function testRecetteById()
+    // {
+    //     $id = 1;
+
+    //     if ($id == null) {
+    //         $this->expectException(InvalidArgumentException::class);
+    //     }
+
+    //     if (!is_int($id)) {
+    //         $this->expectException(InvalidArgumentException::class);
+    //     }
+
+    //     $recette = $this->recetteDAO->recetteById($id);
+
+    //     $this->assertEquals(1, $recette->getId());
+    //     $this->assertEquals('Poulet au curry', $recette->getNomRecette());
+    //     $this->assertEquals('Faire cuire le poulet dans une poele', $recette->getInstruction());
+    //     $this->assertEquals(10, $recette->getTempsPreparation());
+    //     $this->assertEquals(20, $recette->getTempsCuisson());
+    //     $this->assertEquals(1, $recette->getDifficulte());
+    // }
+
+
+}
+
+
+?>
