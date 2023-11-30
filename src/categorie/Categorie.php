73594a7a1e6ad693eff3a2ef99433337ddd5f02e
redes19 <120016236+redes19@users.noreply.github.com>
2023-11-30 11:49:18 +0100

    feat categorie

--- src/categorie/Categorie.php
+++ src/categorie/Categorie.php
@@ -1,3 +1,34 @@
 <?php
 
+class Categorie {
+    // création variables
+    protected $categorie_id;
+    protected $nom_categorie;
+
+    // fonction construct
+    public function __construct($id, $nom){
+        $this->categorie_id = $categorie_id;
+        $this->nom_categorie = $nom_categorie;
+    }
+
+    // Getter
+    public function getCategorieId(){
+        return $this->categorie_id;
+    }
+
+    public function getNomCategorie(){
+        return $this->nom_categorie;
+    }
+
+    // Setter
+    public function setCategorieId($id){
+        $this->categorie_id = $id;
+    }
+
+    public function setNomCategorie($nom){
+        $this->nom_categorie = $nom;
+    }
+
+}
+
 ?>
--- src/categorie/CategorieDAO.php
+++ src/categorie/CategorieDAO.php
@@ -1,3 +1,53 @@
 <?php
 
+class CategorieDAO{
+    private $db;
+
+    public function __construct($db){
+        $this->db = $db;
+    }
+
+     // CREATE
+     public function ajouterCategorie($nomCategorie) {
+        $query = "INSERT INTO categories (nom_categorie) VALUES (:nomCategorie)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':nomCategorie', $nomCategorie, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+
+    // READ
+    public function getAllCategories() {
+        $query = "SELECT * FROM categories";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getCategorieById($categorieId) {
+        $query = "SELECT * FROM categories WHERE categorie_id = :categorieId";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':categorieId', $categorieId, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // UPDATE
+    public function modifierCategorie($categorieId, $nouveauNom) {
+        $query = "UPDATE categories SET nom_categorie = :nouveauNom WHERE categorie_id = :categorieId";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':categorieId', $categorieId, PDO::PARAM_INT);
+        $stmt->bindParam(':nouveauNom', $nouveauNom, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+
+    // DELETE
+    public function supprimerCategorie($categorieId) {
+        $query = "DELETE FROM categories WHERE categorie_id = :categorieId";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':categorieId', $categorieId, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+}
+
 ?>
