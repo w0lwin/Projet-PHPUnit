ec83f8a627b4a1047997163741a89be3ec77bb1b
redes19 <120016236+redes19@users.noreply.github.com>
2023-12-03 16:05:07 +0100

    update css shearchbar

--- src/component/shearchBar.php
+++ src/component/shearchBar.php
@@ -46,18 +46,23 @@ $recettes = shearch();
 
     <!-- Affichage des recettes trouvées -->
     <?php if (!empty($recettes)): ?>
-        <div>
+        <div class="shearch">
             <h2>Résultats de la recherche :</h2>
-            <ul>
-                <?php foreach ($recettes as $recette): ?>
-                    <li>
-                        <a href="detailsRecette.php?id=<?php echo $recette->getId(); ?>">
-                            <h3>Nom: <?php echo $recette->getNomRecette(); ?></h3>
-                            <p>Difficulté: <?php echo $recette->getDifficulte(); ?></p>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <div class="list">
+                <ul class="ul-list" >
+                    <?php foreach ($recettes as $recette): ?>
+                        <li class="li-list">
+                            <a href="detailsRecette.php?id=<?php echo $recette->getId(); ?>">
+                            <div class="shearch-image-recette"><img src="https://assets.afcdn.com/recipe/20160405/45730_w190h190c1cx1000cy1500.webp" alt="image"></div>
+                                <div class="info-shearch-recette">
+                                    <p>Nom:<br> <?php echo $recette->getNomRecette(); ?></p>
+                                    <p>Difficulté:<br> <?php echo $recette->getDifficulte(); ?></p>
+                                </div>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
         </div>
     <?php else: ?>
         <p>Aucune recette trouvée.</p>
